fdcb048f0d67d6f55fa2981ead317f049e7f0d87
Ivica Batinic <ivica.batinic@infinum.hr>
2022-05-14 13:19:51 +0200

    Update styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,38 @@
 import { Box, Flex, Heading } from "@chakra-ui/layout";
+import { SystemProps } from "@chakra-ui/react";
 import { useHeadroom } from "../libs/chakra-ui-headroom";
+
+const unfixedStyles: SystemProps = {
+  borderBottom: "solid 1px",
+  borderBottomColor: "gray.200",
+};
+
+const pinnedStyles: SystemProps = {
+  borderRadius: "16px",
+  boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+  border: "1px solid rgba(255, 255, 255, 0.3)",
+  m: '10px',
+  h: '60px'
+};
 
 export const Header = () => {
   const { getWrapperProps, getInnerProps, status } = useHeadroom();
-
-  console.log(status)
+  const isUnfixed = status === "unfixed";
 
   return (
     <Box {...getWrapperProps()}>
-      <Flex {...getInnerProps()} px={2} h="80px" bg="blue.400" justify="center" align="center">
-        <Heading color="white">Headroom</Heading>
+      <Flex
+        {...getInnerProps()}
+        px={2}
+        h="80px"
+        justify="center"
+        align="center"
+        transition="box-shadow 0.5s ease, height 0.5s ease, margin 0.5s ease"
+        bg="rgba(255, 255, 255, 0.2)"
+        backdropFilter="blur(5px)"
+        {...(isUnfixed ? unfixedStyles : pinnedStyles)}
+      >
+        <Heading>Headroom</Heading>
       </Flex>
     </Box>
   );
